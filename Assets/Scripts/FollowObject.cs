2fa9978bc36e3885910f12a9171589bc0909ef56
David <david.van.deursen@gmail.com>
2025-10-12 12:58:50 +0200

    Added skeletons that follow a target object

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform target;
+    public NavMeshAgent agent;
+    public Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        animator.SetFloat("Velocity", agent.velocity.magnitude);
+    }
+}
