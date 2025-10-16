291b22a8da744fa9d15a893a71b6bc063d37ebb7
Jeremy <jjhotham@gmail.com>
2025-10-16 20:15:36 +0100

    Skeleton die

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     private float _lifetime = 0f;
-    private BoxCollider2D _collider2D;
+    private BoxCollider _collider;
+    [SerializeField] private int _damage = 10;
 
     public void Start()
     {
-        _collider2D = GetComponent<BoxCollider2D>();
+        _collider = GetComponent<BoxCollider>();
     }
     public void Update()
     {
@@ -15,6 +16,21 @@ public class Arrow : MonoBehaviour
         if (_lifetime > 5f)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IEnemy enemy = collision.gameObject.GetComponent<IEnemy>();
+
+        if (enemy != null)
+        {
+            enemy.TakeDamage(_damage);
+
+            Destroy(gameObject);
+        }
+        else
+        {
         }
     }
 }
--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class Skeleton : MonoBehaviour, IEnemy
 {
-    private int _health = 100;
+    [SerializeField]
+    private int _health = 5;
 
     public void TakeDamage(int damage)
     {
         _health -= damage;
+
+        if (_health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
